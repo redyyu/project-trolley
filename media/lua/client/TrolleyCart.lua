8f7b501a0697ad9c23248e8b5451ef6baf74797f
redyyu <redy.ru@gmail.com>
2024-03-30 14:33:33 +0800

    xxx

--- media/lua/client/TrolleyCart.lua
+++ media/lua/client/TrolleyCart.lua
@@ -8,7 +8,7 @@ local Trolley = {}
 
 Trolley.getTrolleysFromInvertory = function (playerInv)
     local trolley_items = {}
-    local items = playerInv:getAllCategory("Container")
+    local items = playerInv:getItemsFromCategory("Container") -- same with getAllCategory("Container")
     for j = 0, items:size() - 1 do
         local item = items:get(j)
         if item:hasTag('Trolley') then
