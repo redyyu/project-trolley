99e16c900bdc6d5d76062763f69e514eeb514b5d
redyyu <redy.ru@gmail.com>
2024-04-16 09:26:58 +0800

    xxx

--- media/lua/server/Items/Distributions_Trolley.lua
+++ media/lua/server/Items/Distributions_Trolley.lua
@@ -5,67 +5,67 @@ local TROLLEY_TYPES = {
 
 for _, item_type in ipairs(TROLLEY_TYPES) do
     table.insert(ProceduralDistributions["list"]["GigamartTools"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["GigamartTools"].items, 2)
+    table.insert(ProceduralDistributions["list"]["GigamartTools"].items, 1)
 
     table.insert(ProceduralDistributions["list"]["CrateCarpentry"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["CrateCarpentry"].items, 2)
+    table.insert(ProceduralDistributions["list"]["CrateCarpentry"].items, 1)
 
     table.insert(ProceduralDistributions["list"]["CrateRandomJunk"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["CrateRandomJunk"].items, 1)
+    table.insert(ProceduralDistributions["list"]["CrateRandomJunk"].items, 0.5)
 
     table.insert(ProceduralDistributions["list"]["CrateTools"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["CrateTools"].items, 0.5)
+    table.insert(ProceduralDistributions["list"]["CrateTools"].items, 0.25)
 
     table.insert(ProceduralDistributions["list"]["DrugShackTools"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["DrugShackTools"].items, 1)
+    table.insert(ProceduralDistributions["list"]["DrugShackTools"].items, 0.5)
 
     table.insert(ProceduralDistributions["list"]["FireStorageTools"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["FireStorageTools"].items, 3)
+    table.insert(ProceduralDistributions["list"]["FireStorageTools"].items, 1.5)
 
     table.insert(ProceduralDistributions["list"]["ForestFireTools"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["ForestFireTools"].items, 2)
+    table.insert(ProceduralDistributions["list"]["ForestFireTools"].items, 1)
 
     table.insert(ProceduralDistributions["list"]["GarageCarpentry"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["GarageCarpentry"].items, 2)
+    table.insert(ProceduralDistributions["list"]["GarageCarpentry"].items, 1)
 
     table.insert(ProceduralDistributions["list"]["GarageTools"].items, item_type);
-    table.insert(ProceduralDistributions["list"]["GarageTools"].items, 1)
+    table.insert(ProceduralDistributions["list"]["GarageTools"].items, 0.5)
 
     table.insert(ProceduralDistributions["list"]["GigamartHousewares"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["GigamartHousewares"].items, 3)
+    table.insert(ProceduralDistributions["list"]["GigamartHousewares"].items, 1.5)
 
     table.insert(ProceduralDistributions["list"]["LoggingFactoryTools"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["LoggingFactoryTools"].items, 3)
+    table.insert(ProceduralDistributions["list"]["LoggingFactoryTools"].items, 1.5)
 
     table.insert(ProceduralDistributions["list"]["ToolStoreCarpentry"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["ToolStoreCarpentry"].items, 2)
+    table.insert(ProceduralDistributions["list"]["ToolStoreCarpentry"].items, 1)
 
     table.insert(ProceduralDistributions["list"]["ToolStoreTools"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["ToolStoreTools"].items, 3)
+    table.insert(ProceduralDistributions["list"]["ToolStoreTools"].items, 1.5)
 
     table.insert(ProceduralDistributions["list"]["ToolStoreMisc"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["ToolStoreMisc"].items, 1)
+    table.insert(ProceduralDistributions["list"]["ToolStoreMisc"].items, 0.5)
 
     table.insert(ProceduralDistributions["list"]["CrateMechanics"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["CrateMechanics"].items, 1)
+    table.insert(ProceduralDistributions["list"]["CrateMechanics"].items, 0.5)
 
     table.insert(ProceduralDistributions["list"]["CrateMetalwork"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["CrateMetalwork"].items, 1)
+    table.insert(ProceduralDistributions["list"]["CrateMetalwork"].items, 0.5)
 
     table.insert(ProceduralDistributions["list"]["StoreCounterBagsFancy"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["StoreCounterBagsFancy"].items, 1)
+    table.insert(ProceduralDistributions["list"]["StoreCounterBagsFancy"].items, 0.5)
 
     table.insert(ProceduralDistributions["list"]["JanitorTools"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["JanitorTools"].items, 1)
+    table.insert(ProceduralDistributions["list"]["JanitorTools"].items, 0.5)
 
     table.insert(ProceduralDistributions["list"]["ToolStoreFarming"].items, item_type)
-    table.insert(ProceduralDistributions["list"]["ToolStoreFarming"].items, 2)
+    table.insert(ProceduralDistributions["list"]["ToolStoreFarming"].items, 1)
 end
 
 
 
 local SPAWN_ROOMS = {
-    ["warehouse"] = 25,
+    ["warehouse"] = 20,
     ["garage_storage"] = 5,
     ["storageunit"] = 5,
 }
